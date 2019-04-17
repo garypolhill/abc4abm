4d1b872a61d3b90e2749ea44fd64b2321a78d8b4
Gary Polhill <gary.polhill@hutton.ac.uk>
2019-04-17 16:06:48 +0100

    Updated qanalyse.sh to be directory independent

--- CEDSS-ABC6-scripts/qanalyse.sh
+++ CEDSS-ABC6-scripts/qanalyse.sh
@@ -7,12 +7,15 @@
 # are 4414 and 16389 tar.bz2 files respectively. It is these that
 # will be reanalysed.
 
-SCRIPT="/mnt/storage/gary/cedss-abc-2019-scripts/analyseCEDSS.pl"
+TOPDIR="/mnt/storage/gary"
+SCRIPTDIR="abc4abm/CEDSS-ABC6-scripts"
+SCRIPT="analyseCEDSS.pl"
+VALFILE="cedss3.3-20190403-Urban-energy-match-totals.csv"
+ERRSCRIPT="energyerr.pl"
 
 # Default to the ABC4 runs
-LS=cedss-abc4.ls
+LS="$TOPDIR/$SCRIPTDIR/cedss-abc4.ls"
 LL4=`wc -l $LS`			# Number of (ABC4) tar files with results in
-ABC=abc4
 RUN=$SGE_TASK_ID
 
 if [[ $RUN -gt $LL4 ]]
@@ -20,8 +23,7 @@ then
     # Run out of ABC4 runs, so switch to ABC6
     
     RUN=$(expr $SGE_TASK_ID - $LL4)
-    ABC=abc6
-    LS=cedss-abc6.ls
+    LS="$TOPDIR/$SCRIPTDIR/cedss-abc6.ls"
 fi
 
 # Pick a tar file from the list
@@ -32,12 +34,19 @@ TAR=`basename $FULTAR`
 ABCDIR=`dirname $FULDIR`
 RUNDIR=`basename $FULDIR`
 
-cd $ABCDIR
+if [[ ! -e "$TOPDIR/$ABCDIR/$VALFILE" ]]
+then
+    cp $TOPDIR/$SCRIPTDIR/$VALFIE $TOPDIR/$ABCDIR
+    cp $TOPDIR/$SCRIPTDIR/$ERRSCRIPT $TOPDIR/$ABCDIR
+fi
+
+
+cd $TOPDIR/$ABCDIR
 
 tar jxf $TAR			# Extract the results in the ABC dir
 				# (the script will wrap them back up
 				# again)
 
-${SCRIPT} -zip $RUNDIR
+$TOPDIR/$SCRIPTDIR/$SCRIPT -zip $RUNDIR
 
 exit 0
